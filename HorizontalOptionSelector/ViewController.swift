068061afe4d546eeff2a02453f5b2ae5ae8e4a9b
Rahul K Rajan <rahulkrajan2016@gmail.com>
2021-08-07 10:43:10 +0530

    Initial Commit

---
+++ HorizontalOptionSelector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HorizontalOptionSelector
+//
+//  Created by Rahul K Rajan on 07/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
